ed80445493d9521d08bcbbd4c73061b4415fa8ee
westshellX_acer <jia.dong.xing@163.com>
2019-09-06 10:38:23 +0800

    osgSky.dll not OK

---
+++ src/osgSky/Version.cpp
@@ -0,0 +1,26 @@
+#include <Version>
+#include <string>
+#include <stdio.h>
+
+const char* osgOceanGetVersion()
+{
+    static char ocean_version[256];
+    static int ocean_version_init = 1;
+    
+    if( ocean_version_init )
+    {
+        if( OSGSKY_VERSION_REVISION == 0 )
+            sprintf( ocean_version, "%d.%d.%d", OSGSKY_VERSION_MAJOR, OSGSKY_VERSION_MINOR, OSGSKY_VERSION_RELEASE );
+        else
+            sprintf( ocean_version, "%d.%d.%d-%d", OSGSKY_VERSION_MAJOR, OSGSKY_VERSION_MINOR, OSGSKY_VERSION_RELEASE, OSGSKY_VERSION_REVISION );
+
+        ocean_version_init = 0;
+    }
+    
+    return ocean_version;
+}
+
+const char* osgOceanGetLibraryName()
+{
+    return "osgOcean Library";
+}
